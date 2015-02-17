231a00bc49c54d2a185f6ddcc95e331979d73272
bentekwork <ben.routson@tekworkforce.com>
2015-02-17 11:05:00 -0500

    First Commit

--- js/app.js
+++ js/app.js
@@ -1,3 +1,12 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
+
+$('#grid').on('click', function(){
+    $('.listView').hide();
+    $('.gridView').show();
+})
+$('#list').on('click', function(){
+    $('.gridView').hide();
+    $('.listView').show();
+})
